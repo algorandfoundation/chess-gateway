0b7d5a75c90e093f8cb8bba9f859ce599cd187fc
Michael J Feher <michael.feher@algorand.foundation>
2026-05-08 15:42:43 -0400

    experimental: fix address parsing

--- src/chain/chain.service.ts
+++ src/chain/chain.service.ts
@@ -83,10 +83,10 @@ export class ChainService {
       unitName: options.unitName,
       assetName: options.assetName,
       url: options.url,
-      manager: Address.fromString(options.managerAddress),
-      reserve: Address.fromString(options.reserveAddress),
-      freeze: Address.fromString(options.freezeAddress),
-      clawback: Address.fromString(options.clawbackAddress),
+      ...(options.managerAddress ? { manager: Address.fromString(options.managerAddress) } : {}),
+      ...(options.reserveAddress ? { reserve: Address.fromString(options.reserveAddress) } : {}),
+      ...(options.freezeAddress ? { freeze: Address.fromString(options.freezeAddress) } : {}),
+      ...(options.clawbackAddress ? { clawback: Address.fromString(options.clawbackAddress) } : {}),
     };
 
     const txnParams: TransactionParams = {
